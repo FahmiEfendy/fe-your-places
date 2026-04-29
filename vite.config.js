b970a872934fb7742ce751d6b58dc395efba4b47
Fahmi Efendy <itsfahmiefendy@gmail.com>
2026-04-29 15:01:25 +0700

    feat: finalize production build assets and configure Vite output settings (rename from .js to .jsx)

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/your-places/',
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
   server: {
     port: 3000,
     proxy: {
